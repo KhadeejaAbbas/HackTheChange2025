d1dfc23495f853dd01c78a87a330f058e358e33a
KhadeejaAbbas <115890004+KhadeejaAbbas@users.noreply.github.com>
2025-11-08 14:48:59 -0700

    added my file

--- backend/text_to_speech.py
+++ backend/text_to_speech.py
@@ -1,11 +1,8 @@
 from openai import OpenAI
 
 client = OpenAI()
-<<<<<<< Updated upstream
-input_audio_file = "/Users/aaronamoso/Desktop/Personal Projects/HackTheChange2025/samplevoice.mp3"
-=======
+
 input_audio_file = "/Users/khadeejaabbas/git/HackTheChange2025/samplevoice.mp3"
->>>>>>> Stashed changes
 audio_file= open(input_audio_file, "rb")
 
 transcription = client.audio.transcriptions.create(
